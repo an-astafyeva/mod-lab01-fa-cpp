7c6bd3f5605212c2c9ad13d1787c60454de23c83
an-astafyeva <astafeva22082004@gmail.com>
2026-03-29 21:51:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
+#include <iostream>
+#include "../include/fun.h"
 
-int main() {
+int main()
+{
+    const char *str = "Hello world test123 ABC def GhI";
+
+    std::cout << "faStr1: " << faStr1(str) << std::endl;
+    std::cout << "faStr2: " << faStr2(str) << std::endl;
+    std::cout << "faStr3: " << faStr3(str) << std::endl;
+
     return 0;
 }
+add main
